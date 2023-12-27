f86dabda74b27eee2d4834ae3289631d874a6555
Pablo Leite <pabloleite03@gmail.com>
2023-12-26 22:05:14 -0300

    Update ValidacoesStringTests.cs

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -12,8 +12,8 @@ public class ValidacoesStringTests
         //TODO: Corrigir a variável "texto" e "resultadoEsperado" da seção Arrange
 
         // Arrange
-        var texto = "Avanade";
-        var resultadoEsperado = 7;
+        var texto = "Matrix";
+        var resultadoEsperado = 6;
 
         // Act
         var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
